e60ee6a5cb1fa413f99df6e8d105205518153c30
lifeng <lifeng@sto.cn>
2021-07-07 11:48:08 +0800

    两数之和

--- 1.两数之和.java
+++ 1.两数之和.java
@@ -7,8 +7,20 @@
 // @lc code=start
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-
-        
+        int[] result = new int[2];
+        Map<Integer,Integer> map = new HashMap<>(nums.length);
+        for(int i=0; i<nums.length;i++) {
+            map.put(nums[i],i);
+        }
+        for(int i=0; i<nums.length;i++) {
+            Integer j = map.get(target-nums[i]);
+            if(j!=null && i!=j) {
+                result[0] = i;
+                result[1] = map.get(target-nums[i]);
+                return result;
+            }
+        }
+        return result;
     }
 }
 // @lc code=end
